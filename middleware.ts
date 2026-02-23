665f9da8cf5a203e5389540d0f30b40cfc2c8d12
azadpoyraz <poyrazdiktir@gmail.com>
2026-02-23 11:42:01 +0300

    Update import path for auth configuration

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
-
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+// auth.config'den değil, içindeki dolu olan config'in olduğu auth.ts'den çekmeli
+import { config as authConfig } from './auth'; 
 
 export const { auth: middleware } = NextAuth(authConfig);
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
+
 
