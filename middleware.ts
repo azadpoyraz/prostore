abe283208c0f45e30a96031a17f9dd56e99821c3
azadpoyraz <poyrazdiktir@gmail.com>
2026-02-23 10:26:39 +0300

    Add route matching configuration for middleware
    Add config for NextAuth middleware to specify route matching.

--- middleware.ts
+++ middleware.ts
@@ -4,3 +4,7 @@ import { authConfig } from './auth.config';
 
 export const { auth: middleware } = NextAuth(authConfig);
 
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
+
