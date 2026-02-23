d26cc08d6b2bdfa7a0f594901fa6e3de854db505
azadpoyraz <poyrazdiktir@gmail.com>
2026-02-23 11:54:06 +0300

    Update middleware to exclude static and API routes
    Exclude static files and API routes from middleware.

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,8 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
-
 export const { auth: middleware } = NextAuth(authConfig);
 
 export const config = {
+  // Statik dosyaları ve API rotalarını hariç tutar
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
-
-
